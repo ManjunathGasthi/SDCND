48079e8ba7464bdc7becdc66a5a461237928543d
unknown <manjunath.gasthi@gmail.com>
2018-08-13 21:48:15 +0530

    files added for UKF project

---
+++ SDCND_Term2/Unscented-Kalman-Filter-Project/src/tools.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    if(estimations.size() == 0 || estimations.size() != ground_truth.size()){
+        cout << "Invalid data" << endl;
+        return rmse;
+    }
+
+    for(unsigned int i=0; i<estimations.size(); ++i){
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array()* residual.array();
+        rmse += residual;
+    }
+
+    // mean
+    rmse /= estimations.size();
+
+    //square root
+    rmse = rmse.array().sqrt();
+
+    return rmse;
+}
